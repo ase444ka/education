57d186f4d92a47d7fd30e09e60ddee4f82a700fb
ase444ka <istomina.asia@yandex.ru>
2020-02-08 16:35:48 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+
+module.exports = {
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    },
+    mode: 'development',
+    entry: {
+        pages: './src/layouts/UI kit/UIKit.js',
+      /*  UIKit: './src/UI kit/UIKit.js'*/
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {sourceMap: true}
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {sourceMap: true}
+                    }
+                ]
+            },
+
+            {
+                test: /\.pug$/,
+                loaders: [
+                    {
+                        loader: "html-loader"
+                    },
+                    {
+                        loader: "pug-html-loader",
+                        options: {
+                            "pretty": true
+                        }
+                    }
+                ]
+            }, {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                loader: "file-loader",
+                options:
+                    {
+                        name: '/images/[name].[ext]'
+                    }
+            },
+            {
+                test: /\.svg$/,
+                exclude: [/fonts/],
+                loader: "file-loader",
+                options:
+                    {
+                        name: '/icons/[name].[ext]'
+                    }
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                include: [/fonts/],
+                loader: "file-loader",
+                options:
+                    {
+                        name: '/fonts/[name].[ext]'
+                    }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        // new HtmlWebpackPlugin({
+        //     filename: "pages.html",
+        //     template: './src/pages/pages.pug'
+        // }),
+        new HtmlWebpackPlugin({
+            filename: "UIKit.html",
+            template: './src/layouts/UI kit/UIKit.pug'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ]
+}
