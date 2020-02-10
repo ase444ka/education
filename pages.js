a5156c342a6206b4daba729a1985864777c696c5
ase444ka <istomina.asia@yandex.ru>
2020-02-10 19:33:46 +0300

    Updates

--- pages.js
+++ pages.js
@@ -77,7 +77,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://https://ase444ka.github.io/Education/UIKit.html";
+/******/ 	__webpack_require__.p = "https://ase444ka.github.io/Education/";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
