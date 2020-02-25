16f02fd84f8d9d63d0ce97bbe42788ec7827d939
Istomina Asya <istomina.asia@yandex.ru>
2020-02-25 23:06:14 +0300

    fixed expandable block

--- src/blocks/expandable/expandable.js
+++ src/blocks/expandable/expandable.js
@@ -1,3 +1,6 @@
-$('.expandable').click(function(){
-    $(this).toggleClass('expandable_expanded');
+$('.expandable i').click(function(){
+    let txt = $(this).text();
+    txt = (txt=='expand_more')? 'expand_less':'expand_more'
+    $(this).parent().toggleClass('expandable_expanded');
+    $(this).text(txt);
 })
