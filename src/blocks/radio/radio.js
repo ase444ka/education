8f4f287e2883fc6038a54259804d5fb7bd812165
Istomina Asya <istomina.asia@yandex.ru>
2020-02-27 10:14:31 +0300

    toggle and radio fixed

--- src/blocks/radio/radio.js
+++ src/blocks/radio/radio.js
@@ -1,3 +1,4 @@
-$('.radio__item').click(function(){
+/* $('.radio__item').click(function(){
+    $('.radio__item .radio__input').removeAttr("checked");
     $(this).find('.radio__input').attr("checked",true);
-})
+}) */
