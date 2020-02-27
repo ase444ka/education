6cb8fd0afe5ccd998792cdaf59a55c507c3c8d4f
Istomina Asya <istomina.asia@yandex.ru>
2020-02-27 22:07:46 +0300

    fixed like button

--- src/blocks/like/like.js
+++ src/blocks/like/like.js
@@ -2,6 +2,7 @@ $(".like").click(function() {
     let checked = "like__button_checked"
     let likes = $(this).find('.like__button').first().text()
     $(this).find('.like__button').toggleClass(checked)
+    $(this).find('.like__base').toggleClass('like__base_checked')
     if ($(this).find('.like__button').hasClass(checked)) $(this).find('.like__button').text(++likes)
     else $(this).find('.like__button').text(--likes)
     return
