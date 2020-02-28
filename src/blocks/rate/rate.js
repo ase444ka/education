30996e609e22d4ac9fedac5478c4964005adc494
Istomina Asya <istomina.asia@yandex.ru>
2020-02-28 15:56:20 +0300

    programmed rate

---
+++ src/blocks/rate/rate.js
@@ -0,0 +1,14 @@
+let click = false;
+$('.rate__star').mouseenter(function(event) {
+    if (!($(this).parent().hasClass("rate_ratable"))) return;
+    $(this).addClass('rate__star_checked');
+    $(this).prevAll().addClass('rate__star_checked');
+    $(this).nextAll().removeClass('rate__star_checked');   
+})
+$('.rate_ratable').mouseleave(function() {
+    if (!click) $(this).children().removeClass('rate__star_checked')
+})
+$('.rate_ratable').mousedown(function() {
+    click = true;
+    $(this).removeClass('rate_ratable');
+})
