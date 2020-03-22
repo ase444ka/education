a5c022759adaa05a7ccead353a898ee9f1dc1c14
Istomina Asya <istomina.asia@yandex.ru>
2020-03-22 17:31:16 +0300

    continue work with datepicker

--- src/blocks/date-diapazon/date-diapazon.js
+++ src/blocks/date-diapazon/date-diapazon.js
@@ -1,11 +1,33 @@
-$('.date-diapazon').datepicker({ 
-    onSelect:  (fd, d, picker) => { 
-        let id = picker.el.id.slice(-2);
-
-        let firstInputId = "#date-diapazon__first_id_" + id
-        let secondInputId = "#date-diapazon__last_id_" + id
-
-        $(firstInputId).val(fd.split("-")[0]);
-        $(secondInputId).val(fd.split("-")[1]);
+class DateDiapazon {
+    constructor(component) {
+      this.component = component;
+      const $start = $('.date-diapazon__start', this.component);
+      const $end = $('.date-diapazon__end', this.component);
+      this._attachEventHandler($start, $end);
     }
+    _attachEventHandler($start, $end) {
+      $start.datepicker({
+        onSelect: function (fd, d, picker) {
+            console.log(fd);
+            $start.val(fd.split("-")[0]);
+            $end.val(fd.split("-")[1]);
+        },
+        clearButton: 'true',
+      });
+      $end.datepicker({
+        onSelect: function (fd, d, picker) {
+            console.log(fd);
+            $start.val(fd.split("-")[0]);
+            $end.val(fd.split("-")[1]);
+        },
+        clearButton: 'true',
+      });
+      
+    }
+  }
+  
+  $(() => {
+    $('.date-diapazon').each((index, node) => {
+      new DateDiapazon(node);
+    });
   });
