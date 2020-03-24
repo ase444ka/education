7388eb59a9f9f0007f63d5c8359c65c8161233dc
Istomina Asya <istomina.asia@yandex.ru>
2020-03-24 18:32:46 +0300

    made date-diapazon

--- src/blocks/date-diapazon/date-diapazon.js
+++ src/blocks/date-diapazon/date-diapazon.js
@@ -1,9 +1,8 @@
+//объявление класса календаря с привязкой по двум инпутам
 class DateDiapazon {
     constructor(component) {
-      this.component = component;
-      const $start = $('.date-diapazon__start', this.component);
-      const $end = $('.date-diapazon__end', this.component);
-      const $endOffset = $end.parent().parent().position().left;
+      const $start = $('.date-diapazon__start', component);
+      const $end = $('.date-diapazon__end', component);
       $(component).datepicker({
         onSelect: function (fd, d, picker) {
             console.log($start);
@@ -14,18 +13,85 @@ class DateDiapazon {
         },
         range: true,
         clearButton: 'true',
-        offset: 0
+        offset: 0,
+        multipleDates: true,
+        clearButton: true,
+        todayButton: true,
       });
+      $('.datepicker-inline', component).addClass('date-diapazon__calendar_hidden');
+      $('.datepicker-inline', component).addClass('date-diapazon__calendar');
+      $('[data-action="clear"]',component).addClass('date-diapazon__calendar__button_target_clear');
+      $('[data-action="today"]',component).addClass('date-diapazon__calendar__button_target_apply');
 
     }
    
   }
-  
+ //инициализация календаря с двойными инпутами на соотв. блоках
   $(() => {
     $('.date-diapazon').each((index, node) => {
       new DateDiapazon(node);
     });
   });
-  $(() => {
+//отображение календаря
+  let dateDiapazonShowCalendar = (event) => {
+    let calendar = $('.datepicker-inline', event.target.closest('.date-diapazon'));
+    if ($(calendar).hasClass('date-diapazon__calendar_hidden')) {
+      $(calendar).removeClass('date-diapazon__calendar_hidden');
+    $( '.date-diapazon__calendar__button_target_apply', event.target.closest('.date-diapazon')).click(dateDiapazonHideCalendar);
+    $( '.date-diapazon__calendar__button_target_clear', event.target.closest('.date-diapazon')).click(dateDiapazonClearDates);
 
-  });
+    }
+    return;
+  };
+  //скрытие календаря
+  let dateDiapazonHideCalendar = (event) => {
+    let parent = event.target.closest('.date-diapazon');
+    let calendar = $('.datepicker-inline', parent);
+    let wrapperExpanded = $('.date-diapazon__input-wrapper_expanded', parent);
+    $(wrapperExpanded).removeClass('date-diapazon__input-wrapper_expanded');  
+    $(calendar).addClass('date-diapazon__calendar_hidden');
+    return;
+  };
+  //очистка инпутов с датами
+  let dateDiapazonClearDates = (event) => {
+    let start = $('.date-diapazon__start', event.target.closest('.date-diapazon'));
+    let end = $('.date-diapazon__end',  event.target.closest('.date-diapazon'));
+    let inRange = event.target.closest('.date-diapazon').querySelectorAll('.-in-range-');
+    inRange.forEach((item) => 
+    {
+      $(item).removeClass('-in-range-')
+      return;
+    }
+    );
+    let selected = event.target.closest('.date-diapazon').querySelectorAll('.-selected-');
+    selected.forEach((item) => 
+    {
+      $(item).removeClass('-selected-')
+      return;
+    }
+    );
+
+    $('.-range-to-').removeClass('-range-to-')
+    $('.-range-from-').removeClass('-range-from-')
+    $(start).val('');
+    $(end).val('');
+    return;
+  };
+//клик на expand_more/less
+  $('.date-diapazon__input-wrapper').click((event) => {
+    //если календарь скрыт - покажем!
+    if ($(event.target.closest('.date-diapazon')).find('.date-diapazon__calendar_hidden').length) {
+      $(event.target).addClass('date-diapazon__input-wrapper_expanded');
+      dateDiapazonShowCalendar(event);
+    }
+    //а если это expand_less - скроем календарь!
+    else if ($(event.target).hasClass('date-diapazon__input-wrapper_expanded')) {
+      dateDiapazonHideCalendar(event);
+    }
+    
+  })
+/* //разворачиваем календарь
+  $('.date-diapazon__start').click(dateDiapazonShowCalendar);
+  //разворачиваем календарь
+  $('.date-diapazon__end').click(dateDiapazonShowCalendar); */
+
