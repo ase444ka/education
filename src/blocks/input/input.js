59e4257e33b46e6c2845f03c2af45dd8b2792208
Istomina Asya <istomina.asia@yandex.ru>
2020-04-28 18:31:50 +0300

    Made script for email checking

--- src/blocks/input/input.js
+++ src/blocks/input/input.js
@@ -2,3 +2,12 @@ import Inputmask from "inputmask";
 $(document).ready(function(){
   Inputmask().mask(document.querySelectorAll(".input_masked"));
 });
+
+$('.input_sending~.input__i').click((event) =>
+{
+  let checkingInput = event.target.previousElementSibling.value;
+  let regecsp = /[-.\w]+@([\w-]+\.)+[\w-]+/;
+  if (regecsp.test(checkingInput)) alert(`Вы будете получать новости на ${checkingInput}`);
+  else alert('Облом :(');
+}
+)
