83a74686ae0aa8e4d17628b61d6e06406e11b14f
Istomina Asya <istomina.asia@yandex.ru>
2020-05-02 10:06:48 +0300

    Made basic calendar's methods

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -1,3 +1,4 @@
+import "./js/datepicker"
 class Calendar {
     constructor(component) {
       $(component).datepicker({
@@ -14,84 +15,53 @@ class Calendar {
       });
     }
 
+    static initialize(_class, block) {
+      $(() => {
+        $(`.${block}`).each((index, node) => {
+          new _class(node);
+        });
+      });
+    }
+
     show(event, block) {
         let calendar = $('.datepicker-inline', event.target.closest(`.${block}`));
         if ($(calendar).hasClass(`${block}__calendar_hidden`)) {
             $(calendar).removeClass(`${block}__calendar_hidden`);
-          $( `.${block}__calendar__button_target_apply`, event.target.closest(`.${block}`)).click(() => this.hide);
-          $( `.${block}__calendar__button_target_clear`, event.target.closest(`.${block}`)).click(() => this.clear);
-      
+            $( `.${block}__calendar__button_target_apply`, event.target.closest(`.${block}`)).click(() => this.hide);
+            $( `.${block}__calendar__button_target_clear`, event.target.closest(`.${block}`)).click(() => this.clear);
           }
           return;
     }
+
+    hide(event, block)  {
+      let parent = event.target.closest(`.${block}`);
+      let calendar = $('.datepicker-inline', parent);
+      let wrapperExpanded = $(`.${block}__input-wrapper_expanded`, parent);
+      $(wrapperExpanded).removeClass(`${block}__input-wrapper_expanded`);  
+      $(calendar).addClass(`${block}__calendar_hidden`);
+      return;
+    }
+
+    clear(event, block)  {
+      let inRange = event.target.closest(`.${block}`).querySelectorAll('.-in-range-');
+      inRange.forEach((item) => 
+      {
+        $(item).removeClass('-in-range-')
+        return;
+      });
+      let selected = event.target.closest(`.${block}`).querySelectorAll('.-selected-');
+      selected.forEach((item) => 
+      {
+        $(item).removeClass('-selected-')
+        return;
+      });
+  
+      $('.-range-to-').removeClass('-range-to-')
+      $('.-range-from-').removeClass('-range-from-')
+      return;
+    }
    
   }
- //инициализация календаря с двойными инпутами на соотв. блоках
-  $(() => {
-    $(`.${block}`).each((index, node) => {
-      new DateDiapazon(node);
-    });
-  });
-//отображение календаря
-  let dateDiapazonShowCalendar = (event) => {
-    let calendar = $('.datepicker-inline', event.target.closest(`.${block}`));
-    
-  };
-  //скрытие календаря
-  let dateDiapazonHideCalendar = (event) => {
-    let parent = event.target.closest(`.${block}`);
-    let calendar = $('.datepicker-inline', parent);
-    let wrapperExpanded = $(`.${block}__input-wrapper_expanded`, parent);
-    $(wrapperExpanded).removeClass(`${block}__input-wrapper_expanded`);  
-    $(calendar).addClass(`${block}__calendar_hidden`);
-    return;
-  };
-  //очистка инпутов с датами
-  let dateDiapazonClearDates = (event) => {
-    let start = $(`.${block}__start`, event.target.closest(`.${block}`));
-    let end = $(`.${block}__end`,  event.target.closest(`.${block}`));
-    let inRange = event.target.closest(`.${block}`).querySelectorAll('.-in-range-');
-    inRange.forEach((item) => 
-    {
-      $(item).removeClass('-in-range-')
-      return;
-    }
-    );
-    let selected = event.target.closest(`.${block}`).querySelectorAll('.-selected-');
-    selected.forEach((item) => 
-    {
-      $(item).removeClass('-selected-')
-      return;
-    }
-    );
-
-    $('.-range-to-').removeClass('-range-to-')
-    $('.-range-from-').removeClass('-range-from-')
-    $(start).val('');
-    $(end).val('');
-    return;
-  };
-//клик на expand_more/less
-  $('.${block}__input-wrapper').click((event) => {
-    //если календарь скрыт - покажем!
-    if ($(event.target.closest(`.${block}`)).find(`.${block}__calendar_hidden`).length) {
-      $(event.target).addClass(`${block}__input-wrapper_expanded`);
-      dateDiapazonShowCalendar(event);
-    }
-    //а если это expand_less - скроем календарь!
-    else if ($(event.target).hasClass(`${block}__input-wrapper_expanded`)) {
-      dateDiapazonHideCalendar(event);
-    }
-    
-  })
   
-  
-  
-  import "./js/datepicker"
-$('.calendar').datepicker({
-    //inline: true,
-    range: true,
-    multipleDates: true,
-    clearButton: true,
-    todayButton: true,
-})
+  Calendar.initialize(Calendar, 'calendar');
+ 
