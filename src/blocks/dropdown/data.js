563989392a40d89413a986b99118c3c15d8f998d
Istomina Asya <istomina.asia@yandex.ru>
2020-05-15 18:04:25 +0300

    Dropdown: limited quantity

--- src/blocks/dropdown/data.js
+++ src/blocks/dropdown/data.js
@@ -9,8 +9,8 @@ export let data = {
         items: {
             "взрослые": {
                 quantity: 0,
-                max_quantity: 100,
-                writing_mode:  function()  {
+                maxQuantity: 100,
+                writingMode:  function()  {
                     if (!this.quantity) return '';
                     if (endDigit(this.quantity, 1)) {
                         return this.quantity + " взрослый";
@@ -20,8 +20,8 @@ export let data = {
             },
             "дети": {
                 quantity: 0,
-                max_quantity: 50,
-                writing_mode:  function()  {
+                maxQuantity: 50,
+                writingMode:  function()  {
                     if (!this.quantity) return '';
                     if (endDigit(this.quantity, 1)) {
                         return this.quantity + " ребенок";
@@ -34,8 +34,8 @@ export let data = {
             },
             "младенцы": {
                 quantity: 0,
-                max_quantity: 30,
-                writing_mode:  function()  {
+                maxQuantity: 3,
+                writingMode:  function()  {
                     if (!this.quantity) return '';
                     if (endDigit(this.quantity, 1)) {
                         return this.quantity + " младенец";
@@ -48,8 +48,8 @@ export let data = {
             },
             "гости": {
                 quantity: 0,
-                max_quantity: 30,
-                writing_mode:  function()  {
+                maxQuantity: 30,
+                writingMode:  function()  {
                     if (!this.quantity) return '';
                     if (endDigit(this.quantity, 1)) {
                         return this.quantity + " гость";
@@ -64,7 +64,7 @@ export let data = {
         result: function() {
             let res;
             this.items["гости"].quantity = this.items["взрослые"].quantity + this.items["дети"].quantity;
-            res = this.items["гости"].writing_mode() + (this.items["младенцы"].writing_mode() && `, ${this.items["младенцы"].writing_mode()}` || '')
+            res = this.items["гости"].writingMode() + (this.items["младенцы"].writingMode() && `, ${this.items["младенцы"].writingMode()}` || '')
             return res;
         },
         resultTotal: function() {
@@ -74,7 +74,7 @@ export let data = {
             if (this.items["дети"].quantity) resArray.push(this.items["дети"]);
             if (this.items["младенцы"].quantity) resArray.push(this.items["младенцы"]);
             for (let item of resArray) {
-                res += item.writing_mode() + ', ';
+                res += item.writingMode() + ', ';
             }
             res = res.slice(0, -2);
             return res;
@@ -83,8 +83,8 @@ export let data = {
     rooms: {
         items: {
             "спальни": {
-                max_quantity: 5,
-                writing_mode: function() {
+                maxQuantity: 5,
+                writingMode: function() {
                     if (endDigit(this.quantity, 1)) {
                         return this.quantity + " спальня";
                     }
@@ -95,8 +95,8 @@ export let data = {
                 }
             },
             "кровати": {
-                max_quantity: 30,
-                writing_mode: function() {
+                maxQuantity: 30,
+                writingMode: function() {
                     if (endDigit(this.quantity, 1)) {
                         return this.quantity + " кровать";
                     }
@@ -107,8 +107,8 @@ export let data = {
                 }
             },
             "ванные комнаты": {
-                max_quantity: 50,
-                writing_mode: function() {
+                maxQuantity: 50,
+                writingMode: function() {
                     if (endDigit(this.quantity, 1)) {
                         return this.quantity + " ванная комната";
                     }
@@ -126,7 +126,7 @@ export let data = {
             if (this.items["кровати"].quantity) resArray.push(this.items["кровати"]);
             if (this.items["ванные комнаты"].quantity) resArray.push(this.items["ванные комнаты"]);
             for (let item of resArray) {
-                res += item.writing_mode() + ', ';
+                res += item.writingMode() + ', ';
             }
             res = res.slice(0, -2);
             return res;
