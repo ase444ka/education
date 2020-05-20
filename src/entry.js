588f92ba537ebbb8db5b131392241e587cbd4986
Istomina Asya <istomina.asia@yandex.ru>
2020-05-20 19:45:02 +0300

    Common reorganizing: pluged fonts
    -renamed 'fonts.css' to 'fonts.scss'
    -imported fonts in entry-file

--- src/entry.js
+++ src/entry.js
@@ -2,5 +2,6 @@ function requireAll(requireContext) {
     return requireContext.keys().map(requireContext);
 }
 
+import 'Fonts/fonts.scss';
 requireAll(require.context('Blocks', true, /\.js$/)); 
 requireAll(require.context('Pages', true, /\.js$/)); 
