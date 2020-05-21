ea2e3c0a2503c3eae665c43d5288229d75146605
Istomina Asya <istomina.asia@yandex.ru>
2020-05-22 00:01:07 +0300

    Impressions: preliminary settings of donat-chart

--- src/blocks/impressions/impressions.js
+++ src/blocks/impressions/impressions.js
@@ -1,1 +1,40 @@
 import './impressions.scss';
+import ApexCharts from 'apexcharts'
+var options = {
+    series: [130, 65, 65],
+    chart: {
+        type: 'donut',
+    },
+    fill: {
+        colors: ['#FFE39C', '#6FCF97', '#BC9CFF'],
+        type: 'gradient',
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    plotOptions: {
+        pie: {
+            customScale: 0.5,
+            donut: {
+                size: "95%",
+            }
+        }
+    },
+        
+
+    
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            chart: {
+                width: 10
+            },
+            legend: {
+                position: 'bottom'
+            }
+        }
+    }]
+};
+
+var chart = new ApexCharts(document.querySelector(".impressions"), options);
+chart.render(); 
