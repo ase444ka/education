618ca564debc1be51e9afd622e9748f94714a8a5
Istomina Asya <istomina.asia@yandex.ru>
2020-05-28 14:46:15 +0300

    range-slider: removed crutch
    found lost context

--- src/blocks/range-slider/range-slider.js
+++ src/blocks/range-slider/range-slider.js
@@ -1,45 +1,8 @@
 import './range-slider.scss';
-/*  //------------------ТАК ПРИ ЗАГРУЗКЕ ОК ПРИ CHANGE НЕ ОК
 import "ion-rangeslider"
 
 let _setDiapason = function()   {
-    let context = this;
-    return function()  {
-        let _from = $(context).prev().find(".irs-from").text();
-        let _to = $(context).prev().find(".irs-to").text();
-        let str = _from + "-" + _to;
-        $(context).parent().prev().html(str);
-        return;
-    }
-};
-
-$(document).ready(function(){
-    $(".js-range-slider").ionRangeSlider({
-        type: "double",
-        min: 1000,
-        max: 15000,
-        from: 5000,
-        to: 10000,
-        skin: "custom",
-        hide_min_max: true,
-        postfix: "&#8381;"
-        // hide_from_to: true
-        
-    });
-    $(".js-range-slider").on("change", _setDiapason.call($(this)));
-    let elems = $(document).find(".js-range-slider");
-    for (let elem of elems) {
-        let boundF = _setDiapason.bind(elem);
-        boundF()();
-    }   
-    
-});  */
-
-//------------------ТАК ПРИ ЗАГРУЗКЕ НЕ ОК ПРИ CHANGE  ОК
-/* import "ion-rangeslider"
-
-let _setDiapason = ()  => {
-    return function() {
+    return () => {
         let _from = $(this).prev().find(".irs-from").text();
         let _to = $(this).prev().find(".irs-to").text();
         let str = _from + "-" + _to;
@@ -47,7 +10,7 @@ let _setDiapason = ()  => {
         return;
     }
 };
- 
+
 $(document).ready(function(){
     $(".js-range-slider").ionRangeSlider({
         type: "double",
@@ -55,60 +18,17 @@ $(document).ready(function(){
         max: 15000,
         from: 5000,
         to: 10000,
+        step: 100,
         skin: "custom",
         hide_min_max: true,
         postfix: "&#8381;"
         // hide_from_to: true
         
     });
-    let elems = document.querySelectorAll(".js-range-slider");
-    for (let elem of elems) {
-        let boundF = _setDiapason.bind(elem);
-        _setDiapason()();
-    }
-    
-    $(".js-range-slider").on("change", _setDiapason.call(this));
-    
-}); */
-
-//---------------------КОСТЫЛЬ, Но надо двигаться дальше хоть как-то!
-import "ion-rangeslider"
-
-let _setDiapason = function()   {
-    let context = this;
-    return function()  {
-        let _from = $(context).prev().find(".irs-from").text();
-        let _to = $(context).prev().find(".irs-to").text();
-        let str = _from + "-" + _to;
-        $(context).parent().parent().find('.field__annotation').html(str);
-        return;
-    }
-};
-
-$(document).ready(function(){
-    $(".js-range-slider").ionRangeSlider({
-        type: "double",
-        min: 1000,
-        max: 15000,
-        from: 5000,
-        to: 10000,
-        skin: "custom",
-        hide_min_max: true,
-        postfix: "&#8381;",
-        step: 100
-        // hide_from_to: true
-        
-    });
-    $(".js-range-slider").on("change", function(){
-        let _from = $(this).prev().find(".irs-from").text();
-        let _to = $(this).prev().find(".irs-to").text();
-        let str = _from + "-" + _to;
-        $(this).parent().parent().find('.field__annotation').html(str);
-    });
+    $(".js-range-slider").on("change", (event) => _setDiapason.call(event.target)());
     let elems = $(document).find(".js-range-slider");
     for (let elem of elems) {
-        let boundF = _setDiapason.bind(elem);
-        boundF()();
+        _setDiapason.call(elem)();
     }   
     
 });
