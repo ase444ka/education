128c948e9b80f8f02aa4168cbddaacddc5417cd3
Istomina Asya <istomina.asia@yandex.ru>
2020-05-30 22:56:35 +0300

    Button-block: made interactive -added data-attribute "target" -added simple script to header-buttons -added hidden registration and login card to main-page

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -2,3 +2,8 @@ import './header.scss';
 $('.header__hamburger').click((event)=>{
     $('.header__nav__item', event.target.closest('.header')).toggleClass('header__nav__item_showing');
 });
+$('.header__button').click((event)=>{
+    $(document.querySelector('.main__room-search')).addClass('main__hidden');
+    let tt = `.main__${event.target.dataset['target']}`;
+    $(document.querySelector(`.main__${event.target.dataset['target']}`)).toggleClass('main__hidden');
+})
